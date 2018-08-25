0b6a60ea6906cc92f275ae1e915b41c2c44e737c
Emily Zarza <emilyzarza@Emilys-Air.lan>
2018-08-25 10:09:50 +1000

    added login form with authentication and spinner

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import firebase from 'firebase';
+import { View } from 'react-native';
+import LoginForm from './components/LoginForm';
 import { Header } from './components/common';
 
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDISmfZFfntel32aMl7kq4MKCzaz_02a3o',
+      authDomain: 'auth-a6b0c.firebaseapp.com',
+      databaseURL: 'https://auth-a6b0c.firebaseio.com',
+      projectId: 'auth-a6b0c',
+      storageBucket: 'auth-a6b0c.appspot.com',
+      messagingSenderId: '389214875981'
+  });
+  }
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <Text>Auth App</Text>
+        <LoginForm />
       </View>
     );
   }
