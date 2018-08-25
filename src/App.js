7311c4f0a3d4a128dc91ff32e8f46d1803b42d40
Emily Zarza <emilyzarza@Emilys-Air.lan>
2018-08-25 14:38:45 +1000

    added logout functionality with firebase.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 import firebase from 'firebase';
 import { View } from 'react-native';
 import LoginForm from './components/LoginForm';
-import { Header } from './components/common';
+import { Header, Button, CardSection, Spinner } from './components/common';
 
 
 class App extends Component {
+  state = { loggedIn: null };
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyDISmfZFfntel32aMl7kq4MKCzaz_02a3o',
@@ -15,15 +17,54 @@ class App extends Component {
       storageBucket: 'auth-a6b0c.appspot.com',
       messagingSenderId: '389214875981'
   });
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      this.setState({ loggedIn: true });
+    } else {
+      this.setState({ loggedIn: false });
+    }
+  });
   }
+
+renderContent() {
+  switch (this.state.loggedIn) {
+    case true:
+    return (
+      <CardSection>
+        <Button onPress={() => firebase.auth().signOut()}>
+          Log out
+        </Button>
+      </CardSection>
+    );
+    case false:
+    return (
+      <LoginForm />
+    );
+    default:
+    return (
+      <CardSection style={styles.spinnerContainer}>
+        <Spinner size="large" />
+      </CardSection>
+    );
+
+  }
+}
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
 }
+
+const styles = {
+  spinnerContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+};
 
 export default App;
